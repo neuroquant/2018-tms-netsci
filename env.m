419ffd881bca8b397e9547c26d917b9d4102c9a8
mnarayan <mnarayan@users.noreply.github.com>
2018-08-29 14:36:28 -0700

    Added setup paths in env.m

--- env.m
+++ env.m
@@ -1,1 +1,17 @@
+% ELECTROME_DIR points to Electrome TMS-EEG Networks
 setenv('ELECTROME_DIR',fullfile(getenv('SCRATCH'),'Electrome'))
+
+% DATADIR points to CC TMS-fMRI preprocessed ROI timeseries
+setenv('DATADIR',['/Volumes/MACBOOKUSB/Datasets/' ...
+                    'tms-fMRI/CC/roitimeseries/']);                
+
+
+NETSCIDIR = fullfile(getenv('HOME'),'MATLAB','netsci');
+addpath(NETSCIDIR) 
+addpath(fullfile(NETSCIDIR,'netsci'));
+addpath(fullfile(NETSCIDIR,'netsci','plot'));
+addpath(fullfile(NETSCIDIR,'netsci','utils'));
+addpath(fullfile(NETSCIDIR,'external','matlab-cliquer')); 
+
+GGMDIR = fullfile(getenv('HOME'),'MATLAB','ggmClass');
+addpath(GGMDIR)
