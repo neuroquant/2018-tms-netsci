241fad823d19e38c748a5e71b547125c63196b16
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2019-02-26 19:52:22 -0800

    Misc. changes for new dataset

--- src/models/ggm_models.m
+++ src/models/ggm_models.m
@@ -1,8 +1,9 @@
 function [ggm_results Shat] = ggm_models()
     
-    DATADIR=['/Volumes/MACBOOKUSB/Datasets/' ...
-                    'tms-fMRI/CC/roitimeseries'];
-    %DATADIR = fullfile('data','interim','CC','roitimeseries');                
+    %DATADIR=fullfile(getenv('CC_DATADIR'),'SchaeferYeo200','roitimeseries');
+    %DATADIR = fullfile('data','interim','CC','roitimeseries');
+    DATADIR=fullfile(getenv('CC_DATADIR'),'Schaefer100_Yeo7');
+                    
     SAVEDIR=fullfile(DATADIR,'ggms');
     mkdir(SAVEDIR);
     ggm_results = struct();
@@ -10,7 +11,7 @@ function [ggm_results Shat] = ggm_models()
     tms_filenames = dir(fullfile(DATADIR,'*.mat'));
     tms_filenames = {tms_filenames.name};
     nconditions = length(tms_filenames);
-    methodname = 'kernelcorr';
+    methodname = 'corr';
     SAVEDIR=fullfile(SAVEDIR,['networktype_' methodname]);
     mkdir(SAVEDIR);
 
@@ -22,7 +23,7 @@ function [ggm_results Shat] = ggm_models()
         [~,tmpfilename] = fileparts(tms_filename);
         savefilename = regexprep(tmpfilename,'collect_roitimeseries','stability_ggms');
         
-        ls(fullfile(SAVEDIR,[savefilename '.mat']))
+        %ls(fullfile(SAVEDIR,[savefilename '.mat']))
         if(exist(fullfile(SAVEDIR,[savefilename '.mat'])))
             sprintf('Loading %s',fullfile(SAVEDIR,savefilename))
             load(fullfile(SAVEDIR,savefilename));
@@ -53,20 +54,35 @@ function [ggm_results Shat] = ggm_models()
             estimate_options.lambda_max = max_edge;
         end
         
+        if(cond(mean(Shat,3))> 5e+3)
+            for cc=1:size(Shat,3)
+                alpha = .05;
+                Shat(:,:,cc) = (1-alpha)*Shat(:,:,cc) + alpha*eye(size(Shat,1));
+            end
+            disp('Checking condition number')
+            cond(mean(Shat,3))';
+        end
+       
+        save(fullfile(SAVEDIR,savefilename),'Shat');
+        
+        
         % shrinkShat = zeros(size(Shat));
         % for cc=1:size(Shat,3)
         %     shrinkage = estimator.stein_shrinkage_sample_covariance(Shat(:,:,cc));
-        %     tau_idx = max(find(shrinkage.tau>.0001));
+        %     tau_idx = max(find(shrinkage.tau>.001));
         %     shrinkShat(:,:,cc) = shrinkage.correlation{tau_idx};
         % end
-        
+        % shrinkage = estimator.stein_shrinkage_sample_covariance(mean(Shat,3))
+        % tau_idx = max(find(shrinkage.tau>.001));
+        % shrinkShat = shrinkage.correlation(tau_idx);
+               
         estimate_options.path = estimate_options.lambdafun(min_edge,max_edge)
         tic;
         ggm_results = estimator.model_average_populations(Shat,estimate_options);
         ggm_time = toc;
         disp(['Time Taken for Condition:' ggm_time])
         ggm_results
-    
+
         save(fullfile(SAVEDIR,savefilename),'-struct','ggm_results');
         save(fullfile(SAVEDIR,savefilename),'Shat','-append');
         
@@ -87,17 +103,25 @@ function Shat = get_cc_correlation_matrices(filename,varargin)
         method = 'corr';
     end
     
+    nTRs = 164;
     studydata = load(filename);
     studydata.condition
     X = studydata.(studydata.Data);
+    Xconfounds = studydata.confounds;
     
     % Get subjects that are either NTHC or TEHC
+    notempty_idx = find(~cellfun(@isempty,X));
     healthy_idx = find(cell2mat(cellfun(@(x)( ...
         ~isempty(strfind(x,'TEHC'))| ~isempty(strfind(x,'NTHC'))), ...
         studydata.subjects,...
-        'UniformOutput',false))); 
+        'UniformOutput',false)));
+    healthy_idx = intersect(healthy_idx,notempty_idx);
     X = X(healthy_idx);
-    [n p] = size(X{1});
+    if(~isempty(Xconfounds))
+        Xconfounds = Xconfounds(healthy_idx);
+    end
+    
+    [~, p] = size(X{1});
     nsubjects = length(X);
     Shat = zeros(p,p,nsubjects);
     
@@ -108,7 +132,7 @@ function Shat = get_cc_correlation_matrices(filename,varargin)
         R = tms_covariate.ts(find(tms_covariate.onsets));
         Runc{1} = tms_covariate.onsets;
         canhrf = getcanonicalhrf(.4,2.4);
-        Y = zeros(n,length(Runc));
+        Y = zeros(nTRs,length(Runc));
         for ll=1:length(Runc)
             Y(:,ll) = conv([zeros(length(canhrf)-1,1); Runc{ll}],canhrf,'valid');
         end
@@ -121,7 +145,7 @@ function Shat = get_cc_correlation_matrices(filename,varargin)
         Runc{5} = tms_covariate.ITI_5;
         Runc{6} = tms_covariate.ITI_6;
         canhrf = getcanonicalhrf(.4,2.4);
-        Y = zeros(n,length(Runc));
+        Y = zeros(nTRs,length(Runc));
         for ll=1:length(Runc)
             Y(:,ll) = conv([zeros(length(canhrf)-1,1); Runc{ll}],canhrf,'valid');
         end
@@ -130,16 +154,29 @@ function Shat = get_cc_correlation_matrices(filename,varargin)
     end 
     
     
-    for cc=1:nsubjects 
-        Xnorm = standardize.successive_normalize(X{cc});
+    for cc=1:nsubjects
+        disp(sprintf('Subject %d',cc))
+        XX = X{cc}; 
+        % Check that session length is exactly nTRs long or chose the last few TRs.
+        if(size(XX,1)>nTRs)
+            XX = XX(end-nTRs+1:end,:);
+        end
+        if(~isempty(Xconfounds))
+            X_confound =  Xconfounds{cc}.X();
+            X_perpY = Xy_orthogonalize(XX, X_confound(end-nTRs+1:end,4:9)); % Only 6 nuisance motion regressors
+            Xnorm = standardize.successive_normalize(X_perpY);
+        else
+            disp('Empty motion regressors')
+            Xnorm = standardize.successive_normalize(XX);
+        end
         switch method
         case 'kernelcorr'
             Shat(:,:,cc) = ...
-                 kernel_correlation(X{cc},Xnorm,[Runc{:}]);
+                 kernel_correlation(XX,Xnorm,[Runc{:}]);
         case 'weightedcorr'
             Shat(:,:,cc) = weighted_correlation(Xnorm,Y);
         otherwise
-            Shat(:,:,cc) = corr(Xnorm); 
+            Shat(:,:,cc) = corr(Xnorm);
         end
     end
 end
@@ -175,9 +212,32 @@ function KerCorrXYX = kernel_correlation(X,Xnorm,Y)
 end
 
 
+function X_perpY =  Xy_orthogonalize(X,Y)
+% Nuisance signal regression
+% X_perpY = Xy_orthogonalize(standardize.standardize_cols(X),options.nuisance);
+
+	% p = size(X,2);
+	% Xy = zeros(size(X));
+	%
+	% for ii=1:p
+	% 	gsProj = gsr'*X(:,ii)/(gsr'*gsr)*gsr;
+	% 	Xy = X(:,ii) - gsProj;
+	% end
+	
+	mu = mean(X); 
+	Xcen = bsxfun(@minus,X,mu); 
+    
+	proj = @(Z)(Z*pinv(Z'*Z)*Z');
+	X_perpY = Xcen - proj(Y)*Xcen;
+	X_perpY = bsxfun(@plus,X_perpY,mu);
+ 
+end
+
+
 function add_paths()
     
     GGMDIR = fullfile(getenv('HOME'),'MATLAB','ggmClass');
+    addpath(fullfile('GGMDIR','solvers','QUIC'));
     run(fullfile(GGMDIR,'setup.m'));
     
 end
