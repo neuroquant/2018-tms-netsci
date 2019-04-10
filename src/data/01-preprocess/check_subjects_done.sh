c9c5b5e7dd716bfd05cbe6c23459384a3becd183
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2019-04-09 20:11:23 -0700

    updated paths

--- src/data/01-preprocess/check_subjects_done.sh
+++ src/data/01-preprocess/check_subjects_done.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+DATADIR=${OAK}/projects/causcon-bids.v1.0
 ALL_SITES=(LaMFG LFp LIFGAnat LIFGBLA LpMFG LVmFp RaMFG RFEF RFp RIFGAnat RIFGBLA RIFJ RIPL RM1 RpMFG RpreSMA RVmFp)
 
 for (( siteno=0; siteno<${#ALL_SITES[@]}; siteno++))
@@ -7,8 +8,10 @@ do
 
     TMS_SITE=${ALL_SITES[$siteno]}
 
-    ls /scratch/groups/aetkin/COMET/CausalConnectome/BIDS/sub-*/ses-d2/func/*.nii.gz | grep ${TMS_SITE} | sed -e "s?.*/sub-??g" | sed -e "s?_ses-d2.*??g" > ${TMS_SITE}_bids.txt
+    ls ${DATADIR}/sub-*/ses-d2/func/*.nii.gz | grep ${TMS_SITE} | sed -e "s?.*/sub-??g" | sed -e "s?_ses-d2.*??g" > ${TMS_SITE}_bids.txt
 
-    ls /scratch/groups/aetkin/COMET/CausalConnectome/derivatives/fmriprep/fmriprep/sub-*/ses-d2/func/*MNI152*preproc_bold.nii.gz | grep ${TMS_SITE} | sed -e "s?.*func/sub-??g" | sed -e "s?_ses-d2.*??g" > ${TMS_SITE}_files.txt
+    ls /scratch/groups/aetkin/COMET/CausalConnectome/derivatives/fmriprep-fsl/fmriprep/sub-*/ses-d2/func/*MNI152*preproc_bold.nii.gz | grep ${TMS_SITE} | sed -e "s?.*func/sub-??g" | sed -e "s?_ses-d2.*??g" > ${TMS_SITE}_files.txt
+
+    ls /scratch/groups/aetkin/COMET/CausalConnectome/derivatives/fmriprep-fsl/fmriprep/sub-*/ses-d2/func/*MELODIC*.tsv | grep ${TMS_SITE} | sed -e "s?.*func/sub-??g" | sed -e "s?_ses-d2.*??g" > ${TMS_SITE}_ica.txt
 
 done
