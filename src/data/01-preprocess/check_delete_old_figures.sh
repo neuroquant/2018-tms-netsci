cf91b340fb0f21aa8e28f8fc29ec47d7b0e56d8e
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2019-04-10 19:00:17 -0700

    Added script to delete outdated reportlets

---
+++ src/data/01-preprocess/check_delete_old_figures.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+OUTPUTDIR=$PI_SCRATCH/COMET/CausalConnectome/derivatives/fmriprep-fsl/fmriprep
+
+echo "find ${OUTPUTDIR}/sub-NTHC*/figures/* -newermt 'Jan 25 00:00' \!  -newermt 'Jan 28 00:00' -print"
+find ${OUTPUTDIR}/sub-NTHC*/figures/* -newermt 'Jan 25 00:00' \!  -newermt 'Jan 28 00:00' -ls
+echo "find ${OUTPUTDIR}/sub-NTHC*/figures/* -newermt 'Jan 25 00:00' \!  -newermt 'Jan 28 00:00' -print"
